eb99dab751df8758bf098ed0bffc2d10172567f1
Arek <arek-karczo.wp.pl@wp.pl>
2025-02-25 21:51:43 +0100

    added cart function, rmv btn function and live price display

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import { menuArray } from "./data.js";
 
-// let cartArry = []
+let cartArray = []
 
 document.addEventListener('click', (e)=> {
-    if ((e.target.id)) {
-        console.log(e.target.id)
-        orderList(e.target.id)
-
+    if (e.target.classList.contains("extMenuBtn")) {      
+        addBtn(e.target.id)
+        renderCart()
     } 
 })
 
+function addBtn(itemId){
+    const selectedItem = menuArray.find(item => item.id === parseInt(itemId));
+
+    if (selectedItem) {
+        cartArray.unshift({
+            name: selectedItem.name,
+            price: selectedItem.price
+        });
+    }
+    
+    console.log(cartArray);
+}
 
 
 function menu() {
@@ -33,59 +44,61 @@ function menu() {
     return menuHtml
 }
 
+function cartMenu() {
+    if (cartArray.length === 0) {
+        return `<div class="yourOrder">Your cart is empty</div>`;
+    }
 
-function orderList (){
-    let cartHtml = ``
+    let cartHtml = `<div class="yourOrder">Your Order</div>`;
 
-    menuArray.map((info) => {
-        cartHtml +=`
-        <div class="yourOrder">Your order</div>
+    const totalPrice = cartArray.reduce((sum, item) => sum + item.price, 0);
+
+    cartArray.forEach((product, index)=> {
+        cartHtml += `
             <div class="orderListItems">
-                
                 <div class="oneItem">
-                    <div id="menuItem">${info.name}</div>
+                    <div id="menuItem">${product.name}</div>
                     <div>
-                        <button class="removeBtn" id="removeBtn">remove</button>
+                        <button class="removeBtn" data-index="${index}">remove</button>
                     </div>  
-                    <div class="price" id="price">$${info.price}</div>
+                    <div class="price" id="price">$${product.price}</div>
                 </div>
-
-                
-                <div class="lineEndOrder"></div>
-                
-                
-                <div class="receipt">
-                    <div class="payValue">Total price:</div>
-                    <div class="totalSum">$$</div>
-                </div>
-                
-                <div class="btn">
-                    <button class="completeOrderBtn">Complete order</button>
-                </div>
-
             </div>
-        `
-    })
-    return cartHtml
+        `;
+    });
+
+    cartHtml += `
+    <div class="lineEndOrder"></div>
+        <div class="receipt">
+            <div class="payValue">Total price:</div>
+            <div class="totalSum">$${totalPrice}</div>
+        </div>
+        <div class="btn">
+            <button class="completeOrderBtn">Complete order</button>
+        </div>
+    `;
+
+    return cartHtml;
 }
 
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("removeBtn")) {
+        const index = parseInt(e.target.getAttribute("data-index"));
+        cartArray.splice(index, 1); // Remove the item
+        renderCart(); // Re-render the cart
+    }
+});
 
 
 
-
-
-
-
-
-
-
-
-
+function renderCart() {
+    document.getElementById("cartMenu").innerHTML = cartMenu();
+}
 
 
 function render (){
     document.getElementById("foodList").innerHTML = menu()
-    // document.getElementById("cartMenu").innerHTML = orderList()
+    
 }
 render()
 
